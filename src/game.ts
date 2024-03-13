8ea66388f545dc5a533c2609dd29de7708206199
Vasilion <lukevasilion@gmail.com>
2024-03-13 01:44:51 -0400

    update image routes, remove public, public serves at root level

--- src/game.ts
+++ src/game.ts
@@ -21,12 +21,12 @@ export default class Game extends Phaser.Scene {
 
   preload() {
     this.load.tilemapTiledJSON("level", "src/Levels/level01.json");
-    this.load.image("tiles", "public/Terrain/Ground/Tilemap_Elevation.png");
-    this.load.image("tiles2", "public/Terrain/Ground/Tilemap_Flat.png");
-    this.load.image("tiles3", "public/Terrain/Ground/Shadows.png");
-    this.load.image("tiles4", "public/Terrain/Water/Foam/Foam.png");
-    this.load.image("tiles5", "public/Terrain/Water/Water.png");
-    this.load.image("tiles6", "public/Terrain/Bridge/Bridge_All.png");
+    this.load.image("tiles", "/Terrain/Ground/Tilemap_Elevation.png");
+    this.load.image("tiles2", "/Terrain/Ground/Tilemap_Flat.png");
+    this.load.image("tiles3", "/Terrain/Ground/Shadows.png");
+    this.load.image("tiles4", "/Terrain/Water/Foam/Foam.png");
+    this.load.image("tiles5", "/Terrain/Water/Water.png");
+    this.load.image("tiles6", "/Terrain/Bridge/Bridge_All.png");
   }
 
   create() {
