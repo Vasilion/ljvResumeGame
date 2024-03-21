fda8e1bf7b3c747971553d5664d1eed186323688
Vasilion <lukevasilion@gmail.com>
2024-03-20 23:37:23 -0400

    animate tiles to turn blue on 'E' input. Display text (filler for work experience art)

--- src/scenes/workInterior.ts
+++ src/scenes/workInterior.ts
@@ -1,39 +1,41 @@
 import Player from "../player";
 export default class WorkInterior extends Phaser.Scene {
   private player: Player;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private map: Phaser.Tilemaps.Tilemap;
+  private tileSet: Phaser.Tilemaps.Tileset[];
+  private baseLayer: Phaser.Tilemaps.TilemapLayer;
+  private propsLayer: Phaser.Tilemaps.TilemapLayer;
   constructor() {
     super("workInterior");
   }
 
   create(): void {
-    const map = this.make.tilemap({ key: "workInterior" });
-    const tileSet = [
-      map.addTilesetImage("Elevation", "tiles"),
-      map.addTilesetImage("exit", "tiles3"),
-      map.addTilesetImage("G_Idle", "tiles12"),
-      map.addTilesetImage("interiors", "tiles10"),
-      map.addTilesetImage("Props", "tiles11"),
+    this.map = this.make.tilemap({ key: "workInterior" });
+    this.tileSet = [
+      this.map.addTilesetImage("Elevation", "tiles"),
+      this.map.addTilesetImage("exit", "tiles3"),
+      this.map.addTilesetImage("G_Idle", "tiles12"),
+      this.map.addTilesetImage("interiors", "tiles10"),
+      this.map.addTilesetImage("Props", "tiles11"),
     ];
 
-    const layerBase = map.createLayer("base", tileSet);
-    layerBase.setCollisionByProperty({ collides: true });
+    this.baseLayer = this.map.createLayer("base", this.tileSet);
+    this.baseLayer.setCollisionByProperty({ collides: true });
 
-    const layerProps = map.createLayer("props", tileSet);
-    layerProps.setCollisionByProperty({ collides: true });
+    this.propsLayer = this.map.createLayer("props", this.tileSet);
+    this.propsLayer.setCollisionByProperty({ collides: true });
 
-    const layerOverProps = map.createLayer("overProps", tileSet);
+    const layerOverProps = this.map.createLayer("overProps", this.tileSet);
     layerOverProps.setCollisionByProperty({ collides: true });
 
     // Use this to animate certain tiles after interaction
 
-    // const layerActiveProps = map.createLayer("activeProps", tileSet);
-    // layerActiveProps.setCollisionByProperty({ collides: true });
-
-    const layerTrigger = map.createLayer("trigger", tileSet);
+    const layerTrigger = this.map.createLayer("trigger", this.tileSet);
     layerTrigger.setCollisionByProperty({ collides: true });
 
     this.player = new Player(this, 640, 600);
-    this.physics.add.collider(this.player, [layerBase, layerProps]);
+    this.physics.add.collider(this.player, [this.baseLayer, this.propsLayer]);
     this.physics.add.collider(
       this.player,
       layerTrigger,
@@ -46,6 +48,7 @@ export default class WorkInterior extends Phaser.Scene {
 
   update(time: number, delta: number): void {
     this.player.setupAnimationsAndMovement();
+    this.interactWithObject(this.map);
   }
 
   private loadWorkGameScene(): void {
@@ -53,6 +56,45 @@ export default class WorkInterior extends Phaser.Scene {
       isFirstLoad: false,
       playerX: 610,
       playerY: 1020,
+    });
+  }
+
+  private standBy(): void {
+    this.scene.resume("workInteior");
+  }
+
+  private interactWithObject(map: Phaser.Tilemaps.Tilemap): void {
+    this.input.keyboard.on("keydown-E", () => {
+      // Convert player's position to tile coordinates
+      const playerTileX = this.propsLayer.worldToTileX(this.player.x);
+      const playerTileY = this.propsLayer.worldToTileY(this.player.y);
+
+      // Get the tile at the player's position
+      const clickedTile = map.getTileAt(
+        playerTileX,
+        playerTileY,
+        true,
+        this.propsLayer
+      );
+
+      // Check if the clicked tile exists and is clickable
+      if (clickedTile && clickedTile.properties.clickable) {
+        console.log(clickedTile);
+        // Perform actions based on the clicked tile
+        const layerActiveProps = map.createLayer("activeProps", this.tileSet);
+        this.cursors = null;
+        this.scene
+          .launch("wordBox", {
+            text: "Resume Art Here.",
+            time: 3000,
+          })
+          .bringToTop("wordBox");
+        this.time.addEvent({
+          delay: 5000,
+          callback: this.standBy,
+          callbackScope: this,
+        });
+      }
     });
   }
 }
